6990974439b0c67b726c6936084a7ff11b630747
Your Name <you@example.com>
2026-04-26 18:09:43 -0500

    Create lib.rs
    Signed-off-by: Your Name <you@example.com>

---
+++ zllg/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod cli;
+pub mod config;
+pub mod dashboard;
+pub mod dashboard_tui;
+pub mod detect;
+pub mod keybind;
+pub mod layout;
+pub mod pane;
+pub mod project_layout;
+pub mod project_pane;
+pub mod tools;
+pub mod workspace;
